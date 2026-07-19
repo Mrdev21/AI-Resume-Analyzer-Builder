c4dcb70c2d30211d765b3a430c6e53371ecc681e
Himanshu Chaudhari <hc102023@gmail.com>
2026-07-20 04:03:14 +0530

    error check in env

--- frontend/src/services/resumeService.js
+++ frontend/src/services/resumeService.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log("API URL:", import.meta.env.VITE_API_URL);
+
 const API = `${import.meta.env.VITE_API_URL}/api/resume`;
 
 export const analyzeResume = async (file) => {
